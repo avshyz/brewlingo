6728c163dfa2693c2f79006f3785da55e3382ad7
avshyz <zarad.avishay@gmail.com>
2025-12-09 01:55:38 +0200

    brand link rework

--- js/page-transitions.js
+++ js/page-transitions.js
@@ -5,16 +5,35 @@ function isBackNavigation() {
   return nav.from?.index > navigation.currentEntry?.index;
 }
 
+// Check if we should use back animation (set by previous page)
+function shouldUseBackAnimation() {
+  const useBack = sessionStorage.getItem('useBackAnimation') === 'true';
+  sessionStorage.removeItem('useBackAnimation');
+  return useBack;
+}
+
+// Mark links with data-back attribute to trigger back-slide animation
+document.addEventListener('click', (e) => {
+  const link = e.target.closest('a[data-back]');
+  if (link) {
+    sessionStorage.setItem('useBackAnimation', 'true');
+  }
+});
+
 window.addEventListener('pageswap', (e) => {
   if (e.viewTransition) {
     e.viewTransition.types.add('slide');
-    if (isBackNavigation()) e.viewTransition.types.add('back');
+    if (isBackNavigation() || sessionStorage.getItem('useBackAnimation') === 'true') {
+      e.viewTransition.types.add('back');
+    }
   }
 });
 
 window.addEventListener('pagereveal', (e) => {
   if (e.viewTransition) {
     e.viewTransition.types.add('slide');
-    if (isBackNavigation()) e.viewTransition.types.add('back');
+    if (isBackNavigation() || shouldUseBackAnimation()) {
+      e.viewTransition.types.add('back');
+    }
   }
 });
